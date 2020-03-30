c626427d77e00a6e12222ab43167b5c67b668b3c
onlineguruweb <onlineguruhosting@gmail.com>
2020-03-30 16:04:24 +0530

    menu-complete

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,71 @@ class _HomepageState extends State<Homepage> {
           IconButton(icon: Icon(Icons.shopping_cart,color: Colors.white), onPressed: (null)),
         ],
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("onlineguruweb"),
+              accountEmail: Text("onlineguruweb@gmail.com"),
+              currentAccountPicture: GestureDetector(
+                              child: CircleAvatar(
+                                backgroundColor: Colors.pink,
+                                child: Icon(Icons.person,color: Colors.white,),
+                  
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue
+              ),
+              ),
+              
+              InkWell(
+                onTap:(){ print("Homepage");},
+                child: ListTile(
+                  leading: Icon(Icons.home,color: Colors.blue,),
+                  title:Text('Homepage'),  
+                ),
+              ),
+              InkWell(
+                onTap:(){ print("account");},
+                child: ListTile(
+                  leading: Icon(Icons.person,color: Colors.blue,),
+                  title:Text('My Account'),  
+                ),
+              ),
+              InkWell(
+                onTap:(){ print("categories");},
+                child: ListTile(
+                  leading: Icon(Icons.dashboard,color: Colors.blue,),
+                  title:Text('Categories'),  
+                ),
+              ),
+               InkWell(
+                onTap:(){ print("wishlist");},
+                child: ListTile(
+                  leading: Icon(Icons.favorite,color: Colors.blue,),
+                  title:Text('My Wishlist'),  
+                ),
+              ),
+              Divider(),
+               InkWell(
+                onTap:(){ print("Settings");},
+                child: ListTile(
+                  leading: Icon(Icons.settings,color: Colors.blue,),
+                  title:Text('Settings'),  
+                ),
+              ),
+               InkWell(
+                onTap:(){ print("Help");},
+                child: ListTile(
+                  leading: Icon(Icons.help,color: Colors.blue,),
+                  title:Text('Support'),  
+                ),
+              ),
+
+          ],
+        )
+      ),
     );
   }
 }
